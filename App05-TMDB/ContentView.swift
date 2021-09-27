3aebcecadd4728552106d077e568856134e29de4
David <A00828702@itesm.mx>
2021-09-27 07:57:35 -0700

    Initial Commit

---
+++ App05-TMDB/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  App05-TMDB
+//
+//  Created by David Josue Marcial Quero on 27/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
